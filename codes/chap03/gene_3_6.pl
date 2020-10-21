d202aec79a72042c9b0233379daffe37c9a59e2a
tokuhira <masatsune@tokuhira.net>
2020-10-22 01:37:04 +0900

    Update gene_3_6.pl
    Fixed from non-negative integers to positive integers.

--- codes/chap03/gene_3_6.pl
+++ codes/chap03/gene_3_6.pl
@@ -1,7 +1,7 @@
 #!perl -w
 use strict;
-sub W_val {          100   }
-sub a_val { int(rand(100)) }
+sub W_val {             100   }
+sub a_val { 1 + int(rand(99)) }
 chomp(my $N = <STDIN>);
 print "$N\n";
 print W_val . "\n";
