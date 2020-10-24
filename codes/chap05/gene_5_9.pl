0ddc8f797d25e0d63d767cd7d6f222d4226bdc7b
tokuhira <masatsune@tokuhira.net>
2020-10-25 01:25:29 +0900

    Create gene_5_9.pl
    The score is pretty rough, but for the time being.

---
+++ codes/chap05/gene_5_9.pl
@@ -0,0 +1,15 @@
+#!perl -w
+use strict;
+chomp(my $N = <STDIN>);
+print "$N\n";
+for (my $r = 0; $r <= $N; ++$r) {
+    for (my $l = 0; $l <= $N; ++$l) {
+        my ($value, $width) = (0, $r - $l);
+	if (0 < $width) {
+	    $value = 1 + int(rand($width + $N));
+	} elsif ($width < 0) {
+	    $value = 1 << 60;
+	}
+	print "$value\n";
+    }
+}
