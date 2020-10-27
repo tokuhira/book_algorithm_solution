f18dc6e8a3e17cae852e4ad088df25d00b423219
tokuhira <masatsune@tokuhira.net>
2020-10-28 02:18:02 +0900

    Update gene_7_1.pl

--- codes/chap07/gene_7_1.pl
+++ codes/chap07/gene_7_1.pl
@@ -2,13 +2,26 @@
 use strict;
 use warnings FATAL => 'all';
 use List::Util qw(sum);
+# コインの定義
+my @coin = (500, 100, 50, 10, 5, 1);
 sub num_base { int(rand(11)) };
 sub num_plus {  int(rand(6)) };
 
 # コインの定義と支払い金額
-my %coin = map { $_ => num_base } (500, 100, 50, 10, 5, 1);
-print sum(map { $_ * $coin{$_} } keys %coin) . "\n";
+my %coin = map { $_ => num_base } @coin;
+my $X = sum(map { $_ * $coin{$_} } @coin);
+print "$X\n";
 
 # プラスした枚数
-$coin{$_} += num_plus for keys %coin;
-print $coin{$_} . "\n" for sort { $b <=> $a } keys %coin;
+$coin{$_} += num_plus for @coin;
+print $coin{$_} . "\n" for @coin;
+
+# 単純解（コイン定義の一般化には未対応）
+my $result = 0;
+for my $coin (@coin) {
+    my $add = int($X / $coin);
+    $add = $coin{$coin} if ($add > $coin{$coin});
+    $X -= $coin * $add;
+    $result += $add;
+}
+warn "$result\n";
