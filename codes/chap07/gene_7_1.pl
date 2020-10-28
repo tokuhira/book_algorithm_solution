f459704892731b1263c3988b756e30476c8d03e7
tokuhira <masatsune@tokuhira.net>
2020-10-28 23:29:46 +0900

    Update gene_7_1.pl
    Added printing the expected solution to standard error.

--- codes/chap07/gene_7_1.pl
+++ codes/chap07/gene_7_1.pl
@@ -2,26 +2,32 @@
 use strict;
 use warnings FATAL => 'all';
 use List::Util qw(sum);
+
 # コインの定義
-my @coin = (500, 100, 50, 10, 5, 1);
-sub num_base { int(rand(11)) };
-sub num_plus {  int(rand(6)) };
+my @type = ( 500, 100, 50, 10, 5, 1 );
+sub num_base { int( rand(11) ) }
+sub num_plus { int( rand(6) ) }
 
 # コインの定義と支払い金額
-my %coin = map { $_ => num_base } @coin;
-my $X = sum(map { $_ * $coin{$_} } @coin);
+my %coin = map { $_ => num_base } @type;
+my $X    = sum( map { $_ * $coin{$_} } @type );
 print "$X\n";
 
-# プラスした枚数
-$coin{$_} += num_plus for @coin;
-print $coin{$_} . "\n" for @coin;
+# 支払いの自由度を上げるため枚数追加
+$coin{$_} += num_plus for @type;
+print $coin{$_} . "\n" for @type;
 
-# 単純解（コイン定義の一般化には未対応）
-my $result = 0;
-for my $coin (@coin) {
-    my $add = int($X / $coin);
-    $add = $coin{$coin} if ($add > $coin{$coin});
-    $X -= $coin * $add;
-    $result += $add;
+# 最小支払い枚数を調べる
+use constant INF => 1 << 29;    # 十分大きな値 (ここでは 2^29 とする)
+sub chmin (\[$]$) { ${ $_[0] } = $_[1] if ${ $_[0] } > $_[1] }
+my @coin = map { ($_) x $coin{$_} } @type;                    # コインを枚数分展開iする
+my $n    = @coin;                                             # コインの枚数
+my $dp   = [ map { [ (INF) x ( $X + 1 ) ] } ( 0 .. $n ) ];    # コインの枚数と支払い金額の DP テーブル
+$dp->[0]->[0] = 0;                                            # 部分和の初期条件
+for ( my $i = 0 ; $i < $n ; ++$i ) {
+    for ( my $j = 0 ; $j <= $X ; ++$j ) {
+        chmin( $dp->[ $i + 1 ]->[$j], $dp->[$i]->[ $j - $coin[$i] ] + 1 ) if $j - $coin[$i] >= 0;
+        chmin( $dp->[ $i + 1 ]->[$j], $dp->[$i]->[$j] );
+    }
 }
-warn "$result\n";
+warn "$dp->[$n]->[$X]\n"; # 想定解を標準エラー出力へ
