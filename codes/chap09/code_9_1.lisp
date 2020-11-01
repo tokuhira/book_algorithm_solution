d3afa027f4bd1fd6039103f7d00218f9988dab35
tokuhira <masatsune@tokuhira.net>
2020-11-02 00:05:06 +0900

    Stack and Queue with Lisp at chap09

---
+++ codes/chap09/code_9_1.lisp
@@ -0,0 +1,49 @@
+(defconstant +MAX+ 100000) ;; スタック配列の最大サイズ 
+
+(defparameter *st* (make-array +MAX+)) ;; スタックを表す配列
+(defparameter *top* 0) ;; スタックの先頭を表す添字
+
+;; スタックを初期化する
+(defun init ()
+  (setf *top* 0)) ;; スタックの添字を初期位置に
+
+;; スタックが空かどうかを判定する
+(defun isEmpty ()
+  (= *top* 0)) ;; スタックサイズが 0 かどうか
+
+;; スタックが満杯かどうかを判定する
+(defun isFull ()
+  (= *top* +MAX+)) ;; スタックサイズが MAX かどうか
+
+;; push
+(defun my-push (x)
+  (cond
+    ((isFull)
+     (format t "error: stack is full.~%"))
+    (t
+     (setf (aref *st* *top*) x) ;; x を格納して
+     (setf *top* (1+ *top*))))) ;; top を進める
+
+;; pop
+(defun my-pop ()
+  (cond
+    ((isEmpty)
+     (format t "error: stack is empty.~%")
+     -1)
+    (t
+     (setf *top* (1- *top*)) ;; top をデクリメントして
+     (aref *st* *top*)))) ;; top の位置にある要素を返す
+
+(defun main ()
+  (init) ;; スタックを初期化
+
+  (my-push 3) ;; スタックに 3 を挿入する {} -> {3}
+  (my-push 5) ;; スタックに 5 を挿入する {3} -> {3, 5}
+  (my-push 7) ;; スタックに 7 を挿入する {3, 5} -> {3, 5, 7}
+
+  (format t "~S~%" (my-pop)) ;; {3, 5, 7} -> {3, 5} で 7 を出力
+  (format t "~S~%" (my-pop)) ;; {3, 5} -> {3} で 5 を出力
+
+  (my-push 9)) ;; 新たに 9 を挿入する {3} -> {3, 9}
+
+(main)
---
+++ codes/chap09/code_9_2.lisp
@@ -0,0 +1,54 @@
+(defconstant +MAX+ 100000) ;; キュー配列の最大サイズ 
+
+(defparameter *qu* (make-array +MAX+)) ;; キューを表す配列
+(defparameter *tail* 0) ;; キューの要素の挿入位置を表す変数
+(defparameter *head* 0) ;; キューの要素の先頭位置を表す変数
+
+;; キューを初期化する
+(defun init ()
+  (setf *tail* 0)
+  (setf *head* 0))
+
+;; キューが空かどうかを判定する
+(defun isEmpty ()
+  (= *head* *tail*))
+
+;; キューが満杯かどうかを判定する
+(defun isFull ()
+  (= *head* (mod (1+ *tail*) +MAX+)))
+
+;; enqueue
+(defun enqueue (x)
+  (cond ((isFull)
+         (format t "error: queue is full.~%"))
+	(t
+	 (setf (aref *qu* *tail*) x)
+	 (setf *tail* (1+ *tail*))
+	 (if (= *tail* +MAX+)
+	     (setf *tail* 0))))) ;; リングバッファの終端に来たら 0 に
+
+;; dequeue
+(defun dequeue ()
+  (cond ((isEmpty)
+         (format t "error: queue is empty.~%")
+         -1)
+	(t
+	 (let ((res (aref *qu* *head*)))
+	   (setf *head* (1+ *head*))
+	   (if (= *head* +MAX+)
+	       (setf *head* 0)) ;; リングバッファの終端に来たら 0 に
+	   res))))
+
+(defun main ()
+  (init) ;; キューを初期化
+
+  (enqueue 3) ;; キューに 3 を挿入する {} -> {3}
+  (enqueue 5) ;; キューに 5 を挿入する {3} -> {3, 5}
+  (enqueue 7) ;; キューに 7 を挿入する {3, 5} -> {3, 5, 7}
+
+  (format t "~S~%" (dequeue)) ;; {3, 5, 7} -> {5, 7} で 3 を出力
+  (format t "~S~%" (dequeue)) ;; {5, 7} -> {7} で 5 を出力
+
+  (enqueue 9)) ;; 新たに 9 を挿入する {7} -> {7, 9}
+
+(main)
