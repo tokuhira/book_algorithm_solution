a5fc65bebbadd6a0b7f32372f096ae26c80bc08c
tokuhira <masatsune@tokuhira.net>
2020-11-02 19:51:05 +0900

    Weighted graph with Lisp at chap10

---
+++ codes/chap10/code_10_4.lisp
@@ -0,0 +1,26 @@
+(defstruct Edge
+  (to) ;; 隣接頂点番号
+  (w)) ;; 重み
+
+(defun main ()
+  (let ((N) (M) (G))
+    ;; 頂点数と辺数
+    (setf N (read))
+    (setf M (read))
+    
+    ;; グラフ
+    (setf G (make-array N))
+    (loop for i below N
+       do (setf (aref G i) (make-array 0 :adjustable t :fill-pointer t)))
+    (loop for i below M
+       do (let ((a (read))
+		(b (read))
+		(w (read)))
+	    (vector-push-extend (make-Edge :to b :w w) (aref G a))))
+
+    ;; デバッグ出力
+    ;(princ G *error-output*)
+    ;(terpri *error-output*)
+    ))
+
+(main)
