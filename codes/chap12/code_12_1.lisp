6aebe5962d1b5cf585637bf40d6a14bebf1f1a88
tokuhira <masatsune@tokuhira.net>
2020-11-06 01:55:49 +0900

    Insertion sort with Lisp at chap12

---
+++ codes/chap12/code_12_1.lisp
@@ -0,0 +1,36 @@
+(defun insertion-sort (a)
+  (loop for i from 1 below (length a)
+     do (let ((v (aref a i))  ;; 挿入したい値
+	      (j i))
+	  
+	  ;; v を挿入する適切な場所 j を探す
+	  (loop
+	     while (> j 0)
+	     do (progn
+		  (if (> (aref a (1- j)) v) ;; v より大きいものは 1 つ後ろに移す
+		      (setf (aref a j) (aref a (1- j)))
+		      (return)) ;;  v 以下になったら止める
+		  (decf j)))
+
+	  (setf (aref a j) v))) ;; 最後に j 番目に v をもってくる
+  a)
+
+(defun main ()
+  (let ((N) (a))
+    ;; 入力
+    (setf N (read)); // 要素数
+    (setf a (make-array N))
+    (loop for i below N
+       do (setf (aref a i) (read)))
+    
+    ;(princ a *error-output*)
+    ;(terpri *error-output*)
+
+    ;; 挿入ソート
+    (insertion-sort a)
+
+    ;(princ a *error-output*)
+    ;(terpri *error-output*)
+    ))
+
+(main)
